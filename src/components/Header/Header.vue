f32f53690a85400893ac0d8428d7876a8f264c1f
Dmitrii Belovodskii <tofupages@gmail.com>
2024-06-29 12:14:37 +0600

    Выполнил отрисовку фильмов (карточек) и иформацию о поиске в зависимости от ввода пользователя

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,13 +2,80 @@
   <header class="header">
     <div class="container header-inner">
       <a href="#" class="logo">Movie Catalog</a>
-      <input class="search" type="text" placeholder="Enter the name of the movie ">
+      <input
+          v-model="searchQuery"
+          @input="handleInput"
+          class="search"
+          type="search"
+          placeholder="Enter the name of the movie "
+      >
       <span class="static-user">Alexander Borisenko</span>
     </div>
   </header>
+  <main class="main">
+    <div class="container">
+      <div class="search-info">
+        <h3
+            v-if="searchQuery.length >= 3"
+        >
+          You searched for: {{ searchQuery }}, {{ totalResults }} result found
+        </h3>
+        <h3 v-else>Enter something in the search to get results</h3>
+      </div>
+      <div class="movies">
+        <div v-for="(movie, index) in movies" :key="index" class="movie">
+          <img class="movie-poster" v-if="movie.Poster !== 'N/A'" :src="movie.Poster" alt="Movie Poster" />
+          <p v-else>No Poster Available</p>
+          <h3>Name: {{ movie.Title }}</h3>
+          <p>Year: {{ movie.Year }}</p>
+          <p>imdbID: {{ movie.imdbID }}</p>
+          <p>Type: {{ movie.Type }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
+  import { ref } from "vue";
+  import axios from "axios";
+  import { API_KEY, API_URL } from "@/constants.js";
+
+  const searchQuery = ref('');
+  const movies = ref([]);
+  const totalResults = ref(0);
+
+  const handleInput = async (ev) => {
+    const input = ev.target.value;
+
+    if (input.trim().length >= 3) {
+      await fetchMovies(input);
+    }
+  };
+
+  const fetchMovies = async (query) => {
+    try {
+      const response = await axios.get(API_URL, {
+        params: {
+          apiKey: API_KEY,
+          s: query
+        }
+      });
+
+      if (response.data.Response === 'True') {
+        movies.value = response.data.Search;
+        totalResults.value = response.data.totalResults;
+      } else {
+        movies.value = [];
+        totalResults.value = 0;
+      }
+
+      console.log(movies.value);
+    } catch (e) {
+      console.error('Error');
+    }
+  };
+
 </script>
 
 <style scoped>
@@ -50,4 +117,29 @@
     width: 15px;
     height: 15px;
   }
+
+  .main {
+    margin: 20px 0;
+  }
+
+  .search-info {
+    font-size: 24px;
+  }
+
+  .movies {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-top: 40px;
+  }
+
+  .movie {
+    width: 21%;
+  }
+
+  .movie-poster {
+    width: 200px;
+    height: auto;
+  }
+
 </style>
